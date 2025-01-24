eda63c78d577a513400622c0eb5e77ed860d9316
alessela <alessa2000@yahoo.com>
2025-01-24 23:36:24 +0200

    add sidebar

---
+++ src/components/SidebarMenu/SidebarMenu.jsx
@@ -0,0 +1,11 @@
+import { Nav } from "react-bootstrap";
+
+const SidebarMenu = () => {
+    return (<Nav className="flex-column">
+        <Nav.Link>Recommended for You</Nav.Link>
+        <Nav.Link>Recently viewed</Nav.Link>
+        <Nav.Link>Favorites</Nav.Link>
+    </Nav>)
+}
+
+export default SidebarMenu;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import { Button } from "react-bootstrap";
+
+const HomePage = () => {
+    return (
+        <div>
+            Hello world!
+            <Button variant="primary">Click me!</Button>
+        </div>
+    )
+}
+
+export default HomePage;
